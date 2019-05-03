498910741669eaf1ed903eda0dc2cb32885af034
Stavros Sykiotis <stavros.sykiotis@campus.tu-berlin.de>
2019-05-03 20:06:16 +0200

    Added FSMC and RainPropagation classes

---
+++ FSMC.h
@@ -0,0 +1,24 @@
+#ifndef FSMC_H
+#define FSMC
+
+namespace rainprop{
+
+class FSMC{
+
+public:
+
+private:
+
+double minval;
+double maxval;
+// snr threshodlds
+// snr recvals
+// psteadystate
+// ptransition
+
+
+};
+}
+
+
+#endif
---
+++ RainPropagation.cpp
@@ -0,0 +1,32 @@
+
+namespace rainprop{
+
+RainPropagation::RainPropagation(double f){
+    this->f = f;
+}
+
+// double RainPropagation::SpecificAttenuation(){
+
+// };
+
+// std::vector<Matrix> RainPropagation::SpecificAttenuation(){
+
+// };
+
+// double RainPropagation::EffectivePathLength(){
+
+// };
+
+// std::vector<Matrix> RainPropagation::EffectivePathLength(){
+
+// };
+
+// double RainPropagation::TotalRainAtt(){
+
+// };
+
+// std::vector<Matrix> RainPropagation::TotalRainAtt(){
+
+// };
+
+}
